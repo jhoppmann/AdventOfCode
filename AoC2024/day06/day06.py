f9fce1512b4a4f7da20a8970b7f1aa80443d2ed6
jhoppmann <jhoppmann@impossiblynamed.de>
2024-12-07 10:42:00 +0100

    Solution for day 6, 2024, part 2

--- AoC2024/day06/day06.py
+++ AoC2024/day06/day06.py
@@ -1,28 +1,55 @@
+from copy import deepcopy
+
+
+def contains_loop(starting_position: tuple, position: tuple, grid: list) -> bool:
+    line = list(grid[position[1]])
+    line[position[0]] = '#'
+    grid[position[1]] = ''.join(line)
+    current_position = starting_position
+    current_direction = 'N'
+    path = set()
+    while True:
+        if will_leave(grid, current_position, current_direction):
+            return False
+        elif next_tile(current_position, current_direction, grid) == '#':
+            current_direction = turn(current_direction)
+        else:
+            current_position = move(current_position, current_direction)
+            if (current_position, current_direction) in path:
+                return True
+            path.add((current_position, current_direction))
+
+
 def run():
     with open('input.txt') as file:
         grid = file.read().splitlines()
 
-    current_position = (0,0)
+    current_position = (0, 0)
     for y in range(0, len(grid)):
         for x in range(0, len(grid[y])):
             if grid[y][x] == '^':
                 current_position = (x, y)
-
-    visited = set()
-    visited.add(current_position)
+    starting_position = current_position
+    path = set()
+    path.add(current_position)
     current_direction = 'N'
 
     while True:
-        print(current_position, current_direction)
         if will_leave(grid, current_position, current_direction):
             break
         elif next_tile(current_position, current_direction, grid) == '#':
             current_direction = turn(current_direction)
         else:
             current_position = move(current_position, current_direction)
-            visited.add(current_position)
+            path.add(current_position)
 
-    print(len(visited))
+    possible_obstacle_positions = 0
+    for position in path:
+        if contains_loop(starting_position, position, deepcopy(grid)):
+            possible_obstacle_positions += 1
+    print('Part 1:', len(path))
+    print('Part 2:', possible_obstacle_positions)
+
 
 def will_leave(grid, current_pos, current_direction):
     if current_direction == 'N' and current_pos[1] == 0:
@@ -34,9 +61,11 @@ def will_leave(grid, current_pos, current_direction):
     if current_direction == 'E' and current_pos[0] == len(grid) - 1:
         return True
 
+
 def next_tile(current_position, current_direction, grid):
     pos = get_next_pos(current_position, current_direction)
     return grid[pos[1]][pos[0]]
+
 
 def move(current_position, current_direction) -> tuple:
     return get_next_pos(current_position, current_direction)
@@ -51,7 +80,6 @@ def get_next_pos(current_position, current_direction) -> tuple:
         return current_position[0], current_position[1] + 1
     if current_direction == 'W':
         return current_position[0] - 1, current_position[1]
-
 
 
 def turn(current_direction: str) -> str:
